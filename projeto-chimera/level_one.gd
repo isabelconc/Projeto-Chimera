d51a84be75ebfbc3a7cc2f2a725ee25a08f7a6a8
laura1050santos <64499700+laura1050santos@users.noreply.github.com>
2025-08-19 08:14:10 -0300

    add pastas
    -assets
    -scripts
    -cenas

--- projeto-chimera/level_one.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-@onready var teste: Button = $teste
-
-func _on_teste_pressed() -> void:
-	get_tree().change_scene_to_file("res://choose_level_screen.tscn")
-	Levelcore.lvl1_unlocked = true
