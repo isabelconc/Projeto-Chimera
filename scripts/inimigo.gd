f8a28dc2d04f8b0800f21316484de1384bc377c0
laura1050santos <64499700+laura1050santos@users.noreply.github.com>
2025-11-17 00:34:26 -0300

    Add files via upload

--- scripts/inimigo.gd
+++ scripts/inimigo.gd
@@ -1,9 +1,47 @@
 extends Node2D
 
-var damage_amount := 50  
+signal enemy_attacked(damage)
+signal enemy_died
+
+var max_hp = 100
+var current_hp = 100
+var attack_damage = 50
+var alive := true
+
+func update_health_bar():
+	if has_node("../HUD/EnemyHP"):
+		var bar: ProgressBar = get_node("../HUD/EnemyHP")
+		bar.value = current_hp
+		if has_node("../HUD/EnemyHP/HPText"):
+			var label: Label = get_node("../HUD/EnemyHP/HPText")
+			label.text = str(current_hp, "/", max_hp)
+			
+func die():
+	if not alive:
+		print("die() chamado mas inimigo já morreu")
+		return
+	print("die() executado!")
+	alive = false
+	emit_signal("enemy_died")
+
+
+func take_damage(amount):
+	if not alive:
+		print("Inimigo já está morto, não toma dano")
+		return false
+	print("Inimigo tomou", amount, "de dano")
+	current_hp -= amount
+	current_hp = max(current_hp, 0)
+	update_health_bar()
+	if current_hp <= 0:
+		print("HP <= 0, chamando die()")
+		die()
+	return current_hp <= 0
+
 
 func attack_player():
-	print("Inimigo atacou! Causou", damage_amount, "de dano.")
-	
-	var game_ref = get_node("../")  
-	game_ref.damage_player(damage_amount)
+	if not alive:
+		print("Inimigo está morto, não ataca")
+		return
+	print("Inimigo atacou com", attack_damage)
+	emit_signal("enemy_attacked", attack_damage)
