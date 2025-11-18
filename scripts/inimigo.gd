17b2f099343bfaf7815d6a80acbbfc4f76d0b5b4
laura1050santos <64499700+laura1050santos@users.noreply.github.com>
2025-11-17 21:29:26 -0300

    Delete scripts directory

--- scripts/inimigo.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-var damage_amount := 50  
-
-func attack_player():
-	print("Inimigo atacou! Causou", damage_amount, "de dano.")
-	
-	var game_ref = get_node("../")  
-	game_ref.damage_player(damage_amount)
